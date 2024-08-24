75577712596ab5586de70510dc8f90f3d066e8f5
VictorFil67 <victorfilinovych@gmail.com>
2024-08-25 00:12:54 +0300

    added burger menu closing by clicking on backdrop or menu item

--- src/mobail.js
+++ src/mobail.js
@@ -1,5 +1,24 @@
-document.querySelector('.burger').addEventListener('click', function () {
-  this.classList.toggle('active');
+document.querySelector('.burger').addEventListener('click', burgerClick);
+document.querySelector('ul').addEventListener('click', handleClick);
+document.querySelector('.hidden').addEventListener('click', backdropClick);
+
+function handleClick(e) {
+  console.log(e.target, e.target.nodeName, e.target.dataset);
+  if (e.target.nodeName === 'A') {
+    burgerClick();
+  }
+}
+
+function backdropClick(e) {
+  console.log(e.target, e.currentTarget);
+  if (e.target === e.currentTarget) {
+    burgerClick();
+  }
+}
+
+function burgerClick() {
+  document.querySelector('.burger').classList.toggle('active');
   document.querySelector('.nav').classList.toggle('open');
+  document.querySelector('.hidden').classList.toggle('none');
   document.querySelector('body').classList.toggle('hidding');
-});
+}
