bee788691b30a0168c65aea56f600a26dee5f78f
MaxBeliienko <mbelienko@gmail.com>
2024-08-26 12:15:49 +0300

    done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+
+// faq
 const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
   item.querySelector('.question').addEventListener('click', () => {
     item.classList.toggle('active');
+  });
+});
+
+// gallery
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    effect: 'slide',
+    slidesPerView: 'auto',
+    speed: 600,
+    breakpoints: {
+      1440: {
+        slidesPerView: 2,
+        spaceBetween: 48,
+      },
+    },
   });
 });
